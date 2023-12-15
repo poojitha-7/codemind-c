dfd0b2ec700e444b14955529e455a599b6614f36
sri poojitha <147791856+poojitha-7@users.noreply.github.com>
2023-12-15 18:38:23 +0530

    #include<stdio.h> int main () {     int n,i,f=0;     scanf("%d",&n);     for(i=1;i<n;i++){         if(n%i==0){             f=f+i;         }     }     if(f==n){         printf("PERFECT");     } else if(f<n){         printf("DEFICIENT");     } else {         printf("ABUNDANT");     } }

---
+++ PDA.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main () {
+    int n,i,f=0;
+    scanf("%d",&n);
+    for(i=1;i<n;i++){
+        if(n%i==0){
+            f=f+i;
+        }
+    }
+    if(f==n){
+        printf("PERFECT");
+    } else if(f<n){
+        printf("DEFICIENT");
+    } else {
+        printf("ABUNDANT");
+    }
+}
