61ec75645589d210e487078232f06dc7794b8dad
sri poojitha <147791856+poojitha-7@users.noreply.github.com>
2023-12-22 22:51:26 +0530

    #include <stdio.h>
    int main() {
        int n;
    
        
        scanf("%d", &n);
    
        for (int i = n; i >=1; i--) {
            for (int j = 1; j <= i; j++) {
                printf("%d", j);
            }
            
            for (int j = i + 1; j <=n; j++) {
                printf("*");
            }
            printf("
    ");
        }
    
       
    }

---
+++ Basic_Pattern_5.4.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main() {
+    int n;
+
+    
+    scanf("%d", &n);
+
+    for (int i = n; i >=1; i--) {
+        for (int j = 1; j <= i; j++) {
+            printf("%d", j);
+        }
+        
+        for (int j = i + 1; j <=n; j++) {
+            printf("*");
+        }
+        printf("
+");
+    }
+
+   
+}
