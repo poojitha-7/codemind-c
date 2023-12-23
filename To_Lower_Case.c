9c91e1e74313888f318b4d9d26f1517ac7b48820
sri poojitha <147791856+poojitha-7@users.noreply.github.com>
2023-12-23 10:06:59 +0530

    #include<stdio.h> int main (){ 	char str[20]; 	int i; 	scanf("%[^ ]s",str); 	for(i=0;str[i]!=NULL;i++){ 	    if(str[i]>=65 && str[i]<=90){ 	    str[i]=str[i]+32; 	} } 	    printf("%s",str);
    }

---
+++ To_Lower_Case.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main (){
+	char str[20];
+	int i;
+	scanf("%[^
+]s",str);
+	for(i=0;str[i]!=NULL;i++){
+	    if(str[i]>=65 && str[i]<=90){
+	    str[i]=str[i]+32;
+	}
+}
+	    printf("%s",str);
+	
+	
+	
+	
+}
