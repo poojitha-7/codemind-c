d9057b5a474d8c224c3892023d230cb19699844b
sri poojitha <147791856+poojitha-7@users.noreply.github.com>
2023-12-28 00:55:10 +0530

    #include<stdio.h> int main () {     int n,c=0,i;     scanf("%d",&n);     int a[n];     for(i=0;i<n;i++){         scanf("%d",&a[i]);     }     int s=0,avg;     for(i=0;i<n;i++){         s+=a[i];     }     avg=s/n;     for(i=0;i<n;i++){         if(a[i]>=avg){             c++;         }     }     printf("%d",c); }

---
+++ Elements_greater_than_average.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main () {
+    int n,c=0,i;
+    scanf("%d",&n);
+    int a[n];
+    for(i=0;i<n;i++){
+        scanf("%d",&a[i]);
+    }
+    int s=0,avg;
+    for(i=0;i<n;i++){
+        s+=a[i];
+    }
+    avg=s/n;
+    for(i=0;i<n;i++){
+        if(a[i]>=avg){
+            c++;
+        }
+    }
+    printf("%d",c);
+}
